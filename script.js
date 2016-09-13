a6da8179a9d4e78d234876c66d5d1be588ea84a3
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-09-13 18:34:47 +0200

    suppression d'un projet

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function myFunction() {
         }
 
 function open_infos() {
-    window.open('elements/images/maquette1.png','Projet1','menubar=no, scrollbars=no, top=100, left=100, width=600, height=500');
+    window.open('elements/images/maquette2.png','Projet1','menubar=no, scrollbars=no, top=100, left=100, width=300, height=500');
 }
 
 //(http://www.supportduweb.com/scripts_tutoriaux-code-source-71-ouvrir-une-nouvelle-fenetre-pop-up-en-javascript-window-open-tutoriel.html)
