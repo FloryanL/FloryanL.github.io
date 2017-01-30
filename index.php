983212cac27e695a86ad24ee16479dbb99f06ca3
Floryan Lollivier <floryan.lollivier@gmail.com>
2017-01-30 13:36:01 +0100

    update porfolio for 'aboutme'

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Portfolio</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="elements/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="elements/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Audiowide|Orbitron:400,700|Bad+Script" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <!--<link rel="stylesheet" type="text/css" href="elements/css/popup.css" />-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body data-offset="1">
+    <header class="container-fluid" id="section1">
+        <div class="col-lg-6 col-lg-push-3 col-sm-8 col-sm-push-2 text-center accueil">
+            <h1>Moi en quelques lignes</h1>
+            <p id="aboutme">Bienvenue sur mon site, je me présente Floryan Lollivier. J'ai acquis un statut de développeur
+                web et mobile au sein de la Code Académie, organisée par FACE (la Fondation Agir Contre l’Exclusion). Je
+                vous propose de cliquer sur "Portfolio" pour en apprendre plus sur mes projets. Pour savoir quels langages
+                j'ai en connaissance, vous pouvez visiter la section "Compétences". Si vous avez des questions ou des
+                commentaires il y a la section "Contact".</p>
+        </div>
+    </header>
+
+    <div class="container-fluid text-center">
+        <nav id="sticky" class="navbar navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+					<!--<a class="navbar-brand" href="#">
+        				<img alt="" src=""></a>-->
+					<a class="navbar-brand name" href="#">F. Lollivier</a>
+
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#test1" 
+					aria-expanded="false" aria-controls="#sticky">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div class="sticky side nav navbar-right col-lg-6 col-lg-pull-1">
+                    <ul class="nav navbar-nav navbar-right" id="test1">
+                        <li class="navpadding js-section"><a href="#section1">Accueil</a></li>
+                        <li class="navpadding js-section"><a href="#section2">Compétences</a></li>
+                        <li class="navpadding js-section"><a href="#section3">Portfolio</a></li>
+                        <li class="navpadding js-section"><a href="#section4">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <section class="container-fluid" id="section2">
+        <div class="col-lg-10 col-lg-push-1 domaine">
+            <h2>Compétences</h2>
+            <p id="langages">Front-end : HTML5 | CSS3 | Javascript | JQuery | Bootstrap | AngularJS | Twig</p>
+            <p id="langages">Back-end : PHP</p>
+            <p id="langages">Outils : Gimp | Photoshop | Github | Visual Studio Code</p>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="section3">
+        <div class="col-lg-10 col-lg-offset-1 xp">
+            <h2>Portfolio</h2>
+            <div class="col-lg-10 col-lg-offset-1">
+                <div>
+
+                    <div class="col-lg-4 col-md-4 text-center">
+                        <img src="elements\images\maquette1.png" class="js-popup" data-target="challenge1" id="maquette1" alt="maquette1" />
+
+                        <!-- modal img  -->
+                        <div class="js-popup-challenge1-content" style="display:none">
+                            <span class="js-popup-close close">x</span>
+                            <img src="elements\images\maquette1.png" data-title="test" class="folio" />
+                            <div id="caption">
+                                <p class="description">Voici le 1er projet que j'ai réalisé <a href="elements/portfolio/Projetcom/Projetcom.html"
+                                        class="linkfolio" target="_blank">ici</a>. <br> (langages utilisés : html & css)</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-4 col-md-4 text-center" id="maquette2" alt="maquette2">
+                        <img src="elements\images\maquette2.png" class="js-popup" data-target="challenge2" />
+
+                        <!-- modal img  -->
+                        <div class="js-popup-challenge2-content" style="display:none">
+                            <span class="js-popup-close close">X</span>
+                            <img src="elements\images\maquette2.png" data-title="test" class="folio" />
+                            <div id="caption">
+                                <p class="description">Voici le 2ème projet que j'ai réalisé <a href="elements/portfolio/Pastime/index.html" class="linkfolio"
+                                        target="_blank">ici</a>. <br> (langages utilisés : html & css)</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 text-center" id="maquette3" alt="maquette3">
+                        <img src="elements\images\maquette3.png" class="js-popup" data-target="challenge3" />
+
+                        <!-- modal img  -->
+                        <div class="js-popup-challenge3-content" style="display:none">
+                            <span class="js-popup-close close">X</span>
+                            <img src="elements\images\maquette3.png" data-title="test" class="folio" />
+                            <div id="caption">
+                                <p class="description">Voici le 3ème projet que j'ai réalisé <a href="elements/portfolio/Projet_bootstrap/index.html" class="linkfolio"
+                                        target="_blank">ici</a>. <br> (langages utilisés : html/css & Bootstrap)</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 text-center" id="123veto" alt="123veto">
+                        <img src="elements\images\123veto.png" class="js-popup" data-target="123veto" />
+
+                        <!-- modal img  -->
+                        <div class="js-popup-123veto-content" style="display:none">
+                            <span class="js-popup-close close">X</span>
+                            <img src="elements\images\123veto.png" data-title="test" class="folio" />
+                            <div id="caption">
+                                <p class="description">Voici un site internet que j'ai réalisé avec d'autres développeurs pour des 
+                                    clients vétérinaires <a href="http://123-veto.fr/" class="linkfolio" target="_blank">ici</a>. 
+                                    <br> (langages utilisés : wordpress & php)</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 text-center" id="Elite-dangerous" alt="Elite-dangerous">
+                        <img src="elements\images\ED2_maquette1.png" class="js-popup" data-target="Elite-dangerous" />
+
+                        <!-- modal img  -->
+                        <div class="js-popup-Elite-dangerous-content" style="display:none">
+                            <span class="js-popup-close close">X</span>
+                            <img src="elements\images\ED2_maquette1.png" data-title="test" class="folio" />
+                            <div id="caption">
+                                <p class="description">Voici le 4ème projet que j'ai réalisé <a href="elements/portfolio/Elite-dangerous/index.html" class="linkfolio" target="_blank">ici</a>. 
+                                    <br> (langages utilisés : html/css & Bootstrap)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+
+    <section class="container-fluid" id="section4">
+        <div class="col-lg-10 col-lg-push-1 contact">
+            <h2>Contact</h2>
+
+            <!-- formulaire -->
+            <form method="post" action="contact.php" class="col-lg-6 col-lg-push-3 col-sm-8 col-sm-push-2">
+                <input type="text" name="nom" id="nom" placeholder="Votre Nom " size="15" maxlength="10" class="text-center col-sm-10 col-sm-push-1" required />
+                <!--<input type="text" name="prenom" id="prenom" placeholder="Votre Prénom" size="15" maxlength="10" class="text-center col-sm-10 col-sm-push-1" required />-->
+                <input type="email" name="email" placeholder="Votre adresse mail" size="18" maxlength="30" class="text-center col-sm-10 col-sm-push-1" required />
+                <textarea name="message" placeholder="Votre commentaire..." rows="3" cols="20" class="col-sm-10 col-sm-push-1" required ></textarea>
+                <button type="submit" class="btnsend col-lg-4 col-lg-push-4 col-sm-4 col-sm-push-4">Envoyer</button>
+            </form>
+        </div>
+    </section>
+
+    <footer class="container-fluid">
+        <div class="col-lg-12">
+            <p class="text-center">
+                <a href="https://twitter.com/floryanlollivi1"><i class="fa fa-twitter fa-2x icon-so" aria-hidden="true"></i></a> 
+                <a href="https://fr.linkedin.com/in/floryan-lollivier-2a4248122"><i class="fa fa-linkedin fa-2x icon-so" aria-hidden="true"></i></a>
+                <a href="https://github.com/FloryanL"><i class="fa fa-github fa-2x icon-so" aria-hidden="true"></i></a> </p>
+            <p class="credits text-center">
+                © 2017 | <a href="mailto:?to=floryan.lollivier@gmail.com" >Me contacter</a> | Développeur web</p>
+        </div>
+    </footer>
+
+</body>
+
+    <script type="text/javascript" src="elements/bootstrap/js/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="elements/js/lib/jquery-ui.min.js"></script>
+    <!--<script type="text/javascript" src="elements/js/lib/jquery.popup.min.js"></script>-->
+    <script type="text/javascript" src="elements/js/script.js"></script>
+
+</html>
